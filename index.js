320cb6051dc4ec7a79baf6ad82e4a55a9c77897c
Henrique Santos <henrique.sespindola@senacsp.edu.br>
2023-06-02 10:29:15 -0300

    Feat: Update e criptografia

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const express = require("express");
 //Importar o módulo mysql
 const mysql = require("mysql2");
 
+//Importar o módulo do bcrypt para criptografia de senha
+const bcrypt = require("bcrypt");
+
+/* ----------------------- Warning ---------------------------------
+Apesar de aceitar uma String, havia um erro na criptografia da senha que foi resolvido da seguinte maneira:
+É preciso converter a quantidade de rounds que estamos passando via env para número. Pois quando passamos com o env,
+Ele é interpretado como String, e o bcrypt não está conseguindo reconhecer o valor. Então para resolver o problema
+foi criada uma constante de saltos que teve seu valor convertido para int.
+*/
+const salt = parseInt(process.env.SALT);
+
 //Instância do express representada por app
 const app = express();
 
@@ -50,15 +61,41 @@ app.get("/users/list", (req, res) => {
     })
 });
 
-
 //Nova rota para criar novos usuários
 app.post("/users/insert", (req, res) => {
-    con.query("INSERT INTO usuario SET ?", [req.body], (error, result) => {
-        if (!error)
-            return res.status(201).send({ output: `Inserted`, data: result });
-        else return res.status(500).send({ output: `Internal error during request process`, erro: error });
-    })
-})
+    //Vamos pegar a senha digitada pelo usuário (FRONT)
+    let sh = req.body.senha;
+    //Realizar a criptograia da senha para então cadastrar no banco
+    bcrypt.hash(sh, salt, (error, result) => {
+        if (!error) {
+            //Devolver a senha porém agora criptografada
+            req.body.senha = result
+            con.query("INSERT INTO usuario SET ?", [req.body], (error, result) => {
+                if (!error)
+                    return res.status(201).send({ output: `Inserted`, data: result });
+                else return res.status(500).send({ output: `Internal error during request process`, erro: error });
+            });
+        }
+        else return res.status(500).send({ output: `Unexpected internal error in password`, erro: error });
+    });
+});
+
+//Criação da rota de atualização de dados
+app.put("/users/update/:id", (req, res) => {
+    let sh = req.body.senha;
+
+    bcrypt.hash(sh, salt, (error, result) => {
+        if (!error) {
+            req.body.senha = result
+            con.query("UPDATE usuario SET ? WHERE idusuario=?", [req.body, req.params.id], (error, result) => {
+                if (!error)
+                    return res.status(202).send({ output: `Updated`, data: result });
+                else return res.status(500).send({ output: `Internal error during request process`, erro: error });
+            });
+        }
+        else return res.status(500).send({ output: `Unexpected internal error in password`, erro: error });
+    });
+});
 
 //Vamos definir a porta de comunicação
 app.listen(process.env.PORT, () => console.log(
